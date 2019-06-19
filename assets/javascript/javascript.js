9f5d07f20c59695f0bbd6bf952687c2d7f423baa
wrsmall <william.r.small89@gmail.com>
2019-06-18 22:16:12 -0400

    just need to add in the timer

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,229 +1,295 @@
-var inncorect = 0;
+var incorrect = 0;
 var correct = 0;
 var unanswered = 5;
+var firstTime = true;
 
 function instruct() {
     $("#question").html("Welcome F1 fans lets test your knowledge of our sport. When the start button is pressed you will have one minute to correctly answer 5 questions");
     $('#remain').html('<button id="start" class="h4 rounded bg-success text-white w-25">' + "Start" + '</button');
+    $(document).on("click", "#start", question1);
 };
 
 function question1() {
     var answerChoice = ["One--1", "Two--2", "Three--3", "Four--4"];
     var correctAnswer = "Three--3";
-    var timer= 15;
-$("#timer").html(timer);
+    var timer = 15;
+    $("#timer").html(timer);
+    $("#remain").empty();
     for (i = 0; i < answerChoice.length; i++) {
         var a = $("<p>");
-        a.addClass("answer text-center h5 rounded mycolor w-75");
+        a.addClass("answer1 text-center m-2 h5 rounded mycolor w-75");
         a.attr("data-name", answerChoice[i]);
         a.text(answerChoice[i]);
         $("#answers").append(a);
     }
-    $(document).on("click", ".answer", checkAnswer);
+    //$(document).on("click", ".answer1", checkAnswer1);
+    if (firstTime) $(document).on("click", ".answer1", checkAnswer1);
 
     $("#question").html("How many tire compounds does Pirelli make avialible for each race?");
-    function checkAnswer() {
+    function checkAnswer1() {
         var userGuess = $(this).attr("data-name");
         if (userGuess === correctAnswer) {
             correct++;
             unanswered--;
-            answrC();
+            answrC1();
         } else {
-            inncorect++;
+            incorrect++;
             unanswered--;
-            answrW();
+            answrW1();
         };
+        console.log(incorrect);
+        console.log(unanswered);
     };
-    function answrC(){ 
+    function answrC1() {
         $("#question").html("Correct!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>")
-        setTimeout( function() {  question2();}, 5000);
-    
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("Pirelli brings " + correctAnswer + " compounds");
+        $("#remain").html("<img src='assets/images/tire-compounds.jpg' width='250px' height='225px'>")
+        setTimeout(function () { question2(); }, 5000);
+
     };
-    function answrW(){
+    function answrW1() {
         $("#question").html("Incorrect!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../assets/images/tire-compounds.jpg'>") 
-        setTimeout( function() {  question2();}, 5000);
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("Pirelli brings " + correctAnswer + " compounds");
+        $("#remain").html("<img src='assets/images/tire-compounds.jpg' width='250px' height='225px'>")
+        setTimeout(function () { question2(); }, 5000);
     };
 };
 function question2() {
     var answerChoice = ["Lance Stroll", "Lewis Hamilton", "Matt Verstappen", "Charles Leclerc"];
     var correctAnswer = "Lewis Hamilton";
+    $("#question").removeClass("text-white h1");
+    $("#question").addClass("text-danger h5 ");
     $("#answers").empty();
     $("#remain").empty();
     for (i = 0; i < answerChoice.length; i++) {
-        var a = $("<button>");
-        a.addClass("answer text-center h5 rounded mycolor w-75");
+        var a = $("<p>");
+        a.addClass("answer2 text-center h5 m-3 rounded mycolor w-75");
         a.attr("data-name", answerChoice[i]);
         a.text(answerChoice[i]);
         $("#answers").append(a);
 
 
     }
-    $(document).on("click", ".answer", checkAnswer);
+    if (firstTime) $(document).on("click", ".answer2", checkAnswer2);
     $("#question").html("Who was last years champion?");
-    function checkAnswer() {
+    function checkAnswer2() {
         var userGuess = $(this).attr("data-name");
         if (userGuess === correctAnswer) {
             correct++;
             unanswered--;
-            answrC();
+            answrC2();
         } else {
-            inncorect++;
+            incorrect++;
             unanswered--;
-            answrW();
+            answrW2();
         };
+        console.log(incorrect);
+        console.log(unanswered);
     };
-    function answrC(){
+    function answrC2() {
         $("#question").html("Correct!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='/assets/images/tire-compounds.jpg'>")
-        setTimeout( function() { question3();}, 5000);
-    
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("Last years champion was " + correctAnswer);
+        $("#remain").html("<img src='assets/images/Lewis-Hamilton.jpg'width='225px' height='225px'>")
+        setTimeout(function () { question3(); }, 5000);
+
     }
-    function answrW(){
+    function answrW2() {
         $("#question").html("Incorrect!!!!");
-        $("#question").addClass("answer text-center h5 rounded mycolor w-75");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>") 
-        setTimeout( function() {  question3();}, 5000);
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("Last years champion was " + correctAnswer);
+        $("#remain").html("<img src='assets/images/Lewis-Hamilton.jpg'width='225px' height='225px'>")
+        setTimeout(function () { question3(); }, 5000);
     };
+
 }
 
 function question3() {
     var answerChoice = ["Ferrari", "Williams", "Mercedes", "McLaren"];
     var correctAnswer = "Ferrari";
+    $("#question").removeClass("text-white h1");
+    $("#question").addClass("text-danger h5 ");
     $("#answers").empty();
     $("#remain").empty();
     for (i = 0; i < answerChoice.length; i++) {
-        var a = $("<button>");
-        a.addClass("answer text-center h5 rounded  mycolor w-75");
+        var a = $("<p>");
+        a.addClass("answer3 text-center h5 m-3 rounded  mycolor w-75");
         a.attr("data-name", answerChoice[i]);
         a.text(answerChoice[i]);
         $("#answers").append(a);
+
     }
-    $(document).on("click", ".answer", checkAnswer);
+    if (firstTime)$(document).on("click", ".answer3", checkAnswer3);
     $("#question").html("What manufacture has the most Constructors' World Championships?");
-    function checkAnswer() {
+    function checkAnswer3() {
         var userGuess = $(this).attr("data-name");
         if (userGuess === correctAnswer) {
             correct++;
             unanswered--;
-            answrC();
+            answrC3();
         } else {
-            inncorect++;
+            incorrect++;
             unanswered--;
-            answrW();
+            answrW3();
         };
+        console.log(incorrect);
+        console.log(unanswered);
     };
-    function answrC(){
+    function answrC3() {
         $("#question").html("Correct!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>")
-        setTimeout( function() { question4();}, 5000);
-    
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html(correctAnswer + " has the most Constructors' World Championships");
+        $("#remain").html("<img src='assets/images/ferrari.jpg'width='225px' height='225px'>")
+        setTimeout(function () { question4(); }, 5000);
+
     }
-    function answrW(){
+    function answrW3() {
         $("#question").html("Incorrect!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>") 
-        setTimeout( function() { question4();}, 5000);
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html(correctAnswer + " has the most Constructors' World Championships");
+        $("#remain").html("<img src='assets/images/ferrari.jpg'width='225px' height='225px'>")
+        setTimeout(function () { question4(); }, 5000);
     };
 };
 
 function question4() {
     var answerChoice = ["Spanish", "Hungarian", "Austrian", "Monaco"];
     var correctAnswer = "Monaco";
+    $("#question").removeClass("text-white h1");
+    $("#question").addClass("text-danger h5 ");
     $("#remain").empty();
     $("#answers").empty();
     for (i = 0; i < answerChoice.length; i++) {
-        var a = $("<button>");
-        a.addClass("answer text-center h5 rounded  mycolor w-75");
+        var a = $("<p>");
+        a.addClass("answer4 text-center h5 rounded m-3  mycolor w-75");
         a.attr("data-name", answerChoice[i]);
         a.text(answerChoice[i]);
         $("#answers").append(a);
 
     }
-    $(document).on("click", ".answer", checkAnswer);
+    if (firstTime) $(document).on("click", ".answer4", checkAnswer4);
     $("#question").html("What Grad Prix is usually held on Memorial Day Weekend?");
-    function checkAnswer() {
+    function checkAnswer4() {
         var userGuess = $(this).attr("data-name");
         if (userGuess === correctAnswer) {
             correct++;
             unanswered--;
-            answrC();
+            answrC4();
         } else {
-            inncorect++;
+            incorrect++;
             unanswered--;
-            answrW();
+            answrW4();
         };
+        console.log(incorrect);
+        console.log(unanswered);
     };
-    function answrC(){
+    function answrC4() {
         $("#question").html("Correct!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>")
-        setTimeout( function() {  question5();}, 5000);
-    
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("The " + correctAnswer + " Grad Prix is usually held on Memorial Day Weekend");
+        $("#remain").html("<img src='assets/images/grand-prix.jpg'width='225px' height='225px'>")
+        setTimeout(function () { question5(); }, 5000);
+
     }
-    function answrW(){
+    function answrW4() {
         $("#question").html("Incorrect!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>") 
-        setTimeout( function() { question5();}, 5000);
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("The " + correctAnswer + " Grad Prix is usually held on Memorial Day Weekend");
+        $("#remain").html("<img src='assets/images/grand-prix.jpg'width='225px' height='225px'>")
+        setTimeout(function () { question5(); }, 5000);
     };
 }
 function question5() {
     var answerChoice = ["0.50 sec", "3.12 sec", "1.92 sec", "2.0 sec"];
     var correctAnswer = "1.92 sec";
+    $("#question").removeClass("text-white h1");
+    $("#question").addClass("text-danger h5 ");
     $("#answers").empty();
     $("#remain").empty();
     for (i = 0; i < answerChoice.length; i++) {
-        var a = $("<button>");
-        a.addClass("answer text-center h5 rounded m-3 mycolor w-75");
+        var a = $("<p>");
+        a.addClass("answer5 text-center h5 rounded m-3 mycolor w-75");
         a.attr("data-name", answerChoice[i]);
         a.text(answerChoice[i]);
         $("#answers").append(a);
 
     }
-    $(document).on("click", ".answer", checkAnswer);
+    if (firstTime) $(document).on("click", ".answer5", checkAnswer5);
     $("#question").html("What is the fastest Pit Stop time ever recorded in F1?");
-    function checkAnswer() {
+    function checkAnswer5() {
         var userGuess = $(this).attr("data-name");
         if (userGuess === correctAnswer) {
             correct++;
             unanswered--;
-            answrC();
+            answrC5();
         } else {
-            inncorect++;
+            incorrect++;
             unanswered--;
-           answrW();
+            answrW5();
         };
+        console.log(incorrect);
+        console.log(unanswered);
     };
-    function answrC(){
+    function answrC5() {
         $("#question").html("Correct!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>")
-    
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("The fastest recorded Pit Stop was " + correctAnswer);
+        $("#remain").html("<img src='assets/images/pit-stop.jpg'width='225px' height='225px'>")
+        setTimeout(function () { results(); }, 5000);
+
     }
-    function answrW(){
+    function answrW5() {
         $("#question").html("Incorrect!!!!");
-        $("#question").addClass("h1 text-white");
-        $("#answers").html("Pirelli brings "+ correctAnswer + " compounds");
-        $("#remain").html("<img src='../images/tire-compounds.jpg'>") 
+        $("#question").removeClass("text-danger h5");
+        $("#question").addClass("text-white h1 ");
+        $("#answers").html("The fastest recorded Pit Stop was " + correctAnswer);
+        $("#remain").html("<img src='assets/images/pit-stop.jpg'width='225px' height='225px'>")
+        setTimeout(function () { results(); }, 5000);
     };
 }
+function results() {
+    $("#remain").empty();
+    $("#question").html("You crossed the finish line your results are as follows");
+    $("#question").removeClass("text-danger h5");
+    $("#question").addClass("text-white h1 ");
+    $("#answers").html("Correct: " + correct + "<br>" +
+        "Incorrect: " + incorrect + "<br>" + "Unanswered " + unanswered)
+    $("#answers").addClass("text-danger h3 test")
+    $("#remain").html("<button id='restart'>Restart</button>")
+    $(document).on("click", "#restart", restart);
 
-question1();
+};
+function restart() {
+    // $(incorrect).val(0);
+    // $(correct).val(0);
+    // $(unanswered).val(5);
+    incorrect = 0;
+    correct = 0;
+    unanswered = 5;
+    console.log(incorrect);
+    console.log(correct);
+    console.log(unanswered);
+    $("#questions").empty();
+    $("#answers").empty();
+    $("#question").removeClass("text-white h1");
+    $("#question").addClass("text-danger h5 ");
+    $("#answers").removeClass("text-danger h3 test");
+    firstTime = false;
+    question1();
+}
+
+instruct();
 
 
 
